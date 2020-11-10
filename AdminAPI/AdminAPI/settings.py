444bc74b9488f058ad0ba5ca9dc892dfe51acee2
lirenjie <lirenjieyeah@yeah.net>
2020-11-10 21:50:08 +0800

    temp commit

--- AdminAPI/AdminAPI/settings.py
+++ AdminAPI/AdminAPI/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'AdminAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin_api',
+        'USER': 'admin',
+        'PASSWORD': 'admin.',
+        'HOST': 'mysql-host',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
